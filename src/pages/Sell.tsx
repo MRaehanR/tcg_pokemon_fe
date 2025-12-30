a9927cf1778690b03191104222cfb62b62a9a899
febri <82074350+febri-dot@users.noreply.github.com>
2025-12-30 17:23:02 +0700

    feat: add sell condition

--- src/pages/Sell.tsx
+++ src/pages/Sell.tsx
@@ -21,7 +21,7 @@ const Sell: React.FC = () => {
       try {
         const token = localStorage.getItem("access_token");
         
-        const response = await fetch('http://127.0.0.1:5000/api/v1/profile/get', {
+        const response = await fetch('http://127.0.0.1:5000/api/v1/profile/get?sell=true', {
           method: 'GET',
           headers: {
             'Content-Type': 'application/json',
